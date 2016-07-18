f51bb313b5e47ce899eef17f426ef2d403b209ac
Dmitry Shurunov <d.shurunov@gmail.com>
2016-07-18 19:09:02 +0300

    Реализованы методы для создания\обновления\удаления EventLink

--- src/methods/EventLinkMethod.php
+++ src/methods/EventLinkMethod.php
@@ -2,6 +2,7 @@
 
 namespace LeMaX10\MeetingatSocketClient\methods;
 
+use App\Models\v1\EventParticipant;
 use LeMaX10\MeetingatSocketClient\Enums\SendTypeEnum;
 
 class EventLinkMethod implements MethodInterface
@@ -26,5 +27,37 @@ class EventLinkMethod implements MethodInterface
 	public function delete($ids, array $params = [])
 	{
 		return $this->send(SendTypeEnum::DELETE, 'eventLink/:ids', ['params' => ['ids' => $ids], 'body' => $params]);
+	}
+
+	public static function checkAndUpdate(EventParticipant $ep)
+	{
+		$findEventLink = app('SocketClient')->eventLink->get($ep->user_id . ':' . $ep->event_id);
+		$findUser = app('SocketClient')->users->get($ep->user_id);
+
+		if ($findEventLink) {
+			$attributes = [
+				'rethink_id' => $findUser->body->user_id,
+				'sendPush' => 0,
+				'sendToApi' => 1,
+				'status' => $ep->status
+			];
+
+			if ($findEventLink->code == 404) {
+				$attributes = array_merge($attributes, [
+					'event_id' => $ep['event_id'],
+					'user_id' => $ep['user_id']
+				]);
+				app('SocketClient')->eventLink->create($attributes);
+			} else if ($findEventLink->code == 200) {
+				app('SocketClient')->eventLink->update($ep->user_id . ':' . $ep->event_id, $attributes);
+			}
+		}
+	}
+
+	public static function checkAndDelete(EventParticipant $ep)
+	{
+		$findEvenLink = app('SocketClient')->eventLink->get($ep->user_id . ':' . $ep->event_id);
+		if ($findEvenLink->code == 200)
+			app('SocketClient')->eventLink->delete($ep->user_id . ':' . $ep->event_id);
 	}
 }
