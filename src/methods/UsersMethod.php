3d8d3a9f4ab29fe9e74a5425a217d6dc72a9a739
Владимир Пьянков <rdlrobot@gmail.com>
2016-07-20 11:30:59 +0300

    Users and Images

--- src/methods/UsersMethod.php
+++ src/methods/UsersMethod.php
@@ -28,4 +28,27 @@ class UsersMethod implements MethodInterface
     {
         return $this->send(SendTypeEnum::PUT, 'users/:id', ['params' => ['id' => $userId], 'body' => $params]);
     }
+    
+    public static function checkAndUpdate($user)
+    {
+        $findUser = app('SocketClient')->users->get($user->id);
+        if($findUser) {
+            $attributes = [
+                'lastName' => $user->last_name,
+                'firstName'=> $user->first_name,
+                'user_id'  => (int) $user->id,
+                'created_at' => (string) $user->created_at,
+                'updated_at' => (string) $user->updated_at,
+                'moderated'  => $user->getIsModeratedOnceAttribute() ? 1 : 0
+            ];
+
+            if ($findUser->code == 404) {
+                app('SocketClient')->users->create($attributes);
+                app('SocketClient')->images->checkAndUpdateOrDelete($user);
+            } else if($findUser->code == 200) {
+                app('SocketClient')->users->update($user->id, $attributes);
+                app('SocketClient')->images->checkAndUpdateOrDelete($user);
+            }
+        }
+    }
 }
