1d1237a3e873886821f7d98b2d6b6577af1b1f66
LeMaX10 <rdlrobot@gmail.com>
2017-03-21 17:23:49 +0300

    Update MessageMethod.php
    new method for MessageMethod

--- src/methods/MessageMethod.php
+++ src/methods/MessageMethod.php
@@ -1,25 +1,41 @@
 <?php namespace LeMaX10\MeetingatSocketClient\methods;
-
 use LeMaX10\MeetingatSocketClient\Enums\SendTypeEnum;
-
+use Unirest\Response;
 class MessageMethod implements MethodInterface
 {
 	use MethodTrait;
-
 	public function get(string $ids)
 	{
 		return $this->send(SendTypeEnum::GET, 'messages/:ids', ['params' => ['ids' => $ids]]);
 	}
-
+	public function lastMessages(int $userId)
+	{
+		return $this->send(SendTypeEnum::GET, 'messages/chats/:userId/lastMessages', ['params' => ['userId' => $userId]]);
+	}
+	/**
+	 * @param int $userId
+	 * @param int $eventId
+	 * @return bool
+	 */
 	public static function getMessages(int $userId, int $eventId)
 	{
 		$findUser = $userId ? app('SocketClient')->users->get($userId) : false;
 		$findEvent = $eventId ? app('SocketClient')->eventLink->get($eventId) : false;
 		if (!$findUser && !$findEvent)
 			return false;
-
 		$ids = join(':', [$userId, $eventId]);
-
 		return app('SocketClient')->messages->get($ids);
+	}
+	/**
+	 * @param int $userId
+	 * @return Response
+	 * @throws \Exception
+	 */
+	public static function getLastChatMessages(int $userId) : Response
+	{
+		$findUser = $userId ? app('SocketClient')->users->get($userId) : false;
+		if (!$findUser)
+			throw new \Exception('Пользователь не найден', 500);
+		return app('SocketClient')->messages->lastMessages($userId);
 	}
 }
